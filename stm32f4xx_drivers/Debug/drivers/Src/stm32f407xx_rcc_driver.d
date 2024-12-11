8652a449705b9498dbccae19f57cd43f50b67aca
Ganasri S <ganasri.s@mistralsolutions.com>
2024-12-11 18:23:14 +0530

    UART Driver development complete

--- stm32f4xx_drivers/Debug/drivers/Src/stm32f407xx_rcc_driver.d
+++ stm32f4xx_drivers/Debug/drivers/Src/stm32f407xx_rcc_driver.d
@@ -1,13 +1,13 @@
-drivers/Src/stm32f407xx_spi_driver.o: \
- ../drivers/Src/stm32f407xx_spi_driver.c \
- C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h \
+drivers/Src/stm32f407xx_rcc_driver.o: \
+ ../drivers/Src/stm32f407xx_rcc_driver.c \
+ C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_uart_driver.h \
  C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h
-C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
+C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_rcc_driver.h:
 C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
 C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
 C:/Users/ganasri.s/STM32CubeIDE/workspace_1.16.0/stm32f4xx_drivers/drivers/Inc/stm32f407xx_spi_driver.h:
--- stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
@@ -208,6 +208,36 @@ typedef struct
 	__vo uint32_t CFGR;
 }SYSCFG_RegDef_t;
 
+/*
+ * Peripheral register definition structure for I2C
+ */
+typedef struct
+{
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t OAR1;
+	__vo uint32_t OAR2;
+	__vo uint32_t DR;
+	__vo uint32_t SR1;
+	__vo uint32_t SR2;
+	__vo uint32_t CCR;
+	__vo uint32_t TRISE;
+	__vo uint32_t FLTR;
+}I2C_RegDef_t;
+
+/*
+ * peripheral register definition structure for USART
+ */
+typedef struct
+{
+	__vo uint32_t SR;
+	__vo uint32_t DR;
+	__vo uint32_t BRR;
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t CR3;
+	__vo uint32_t GTPR;
+} USART_RegDef_t;
 
 /*
  * Peripheral definitions (Peripheral Base Addresses typecasted to xxx_RegDef_t)
@@ -233,6 +263,17 @@ typedef struct
 #define SPI2     ((SPI_RegDef_t*) SPI2_BASEADDR)
 #define SPI3     ((SPI_RegDef_t*) SPI3_BASEADDR)
 #define SPI4     ((SPI_RegDef_t*) SPI4_BASEADDR)
+
+#define I2C1    ((I2C_RegDef_t*) I2C1_BASEADDR)
+#define I2C2    ((I2C_RegDef_t*) I2C2_BASEADDR)
+#define I2C3    ((I2C_RegDef_t*) I2C3_BASEADDR)
+
+#define USART1  			((USART_RegDef_t*)USART1_BASEADDR)
+#define USART2  			((USART_RegDef_t*)USART2_BASEADDR)
+#define USART3  			((USART_RegDef_t*)USART3_BASEADDR)
+#define UART4  				((USART_RegDef_t*)UART4_BASEADDR)
+#define UART5  				((USART_RegDef_t*)UART5_BASEADDR)
+#define USART6  			((USART_RegDef_t*)USART6_BASEADDR)
 
 /*
  * Clock Enable Macros for GPIOx peripherals
@@ -357,6 +398,17 @@ typedef struct
 #define IRQ_NO_EXTI4         10
 #define IRQ_NO_EXTI9_5       23
 #define IRQ_NO_EXTI15_10     40
+#define IRQ_NO_SPI1			 35
+#define IRQ_NO_SPI2          36
+#define IRQ_NO_SPI3          51
+#define IRQ_NO_I2C1_EV       31
+#define IRQ_NO_I2C1_ER       32
+#define IRQ_NO_USART1	     37
+#define IRQ_NO_USART2	     38
+#define IRQ_NO_USART3	     39
+#define IRQ_NO_UART4	     52
+#define IRQ_NO_UART5	     53
+#define IRQ_NO_USART6	     71
 
 /*
  * Macros for all the possible priority levels
@@ -426,11 +478,147 @@ typedef struct
 /*
  * Macros to reset SPIx peripherals
  */
-#define SPI1_REG_RESET()      do{ (RCC->APB2ENR |= (1 << 12)); (RCC->APB1RSTR &= ~(1 << 12)); }while(0)
+#define SPI1_REG_RESET()      do{ (RCC->APB2ENR |= (1 << 12)); (RCC->APB2RSTR &= ~(1 << 12)); }while(0)
 #define SPI2_REG_RESET()      do{ (RCC->APB1ENR |= (1 << 14)); (RCC->APB1RSTR &= ~(1 << 14)); }while(0)
 #define SPI3_REG_RESET()      do{ (RCC->APB1ENR |= (1 << 15)); (RCC->APB1RSTR &= ~(1 << 15)); }while(0)
 #define SPI4_REG_RESET()      do{ (RCC->APB2ENR |= (1 << 13)); (RCC->APB2RSTR &= ~(1 << 13)); }while(0)
 
+/***********************************************************************
+ * Bit position definitions of I2C peripheral
+ ***********************************************************************
+ * Bit position definitions I2C_CR1
+ */
+#define I2C_CR1_PE						0
+#define I2C_CR1_NOSTRETCH  				7
+#define I2C_CR1_START 					8
+#define I2C_CR1_STOP  				 	9
+#define I2C_CR1_ACK 				 	10
+#define I2C_CR1_SWRST  				 	15
+
+/*
+ * Bit position definitions I2C_CR2
+ */
+#define I2C_CR2_FREQ				 	0
+#define I2C_CR2_ITERREN				 	8
+#define I2C_CR2_ITEVTEN				 	9
+#define I2C_CR2_ITBUFEN 			    10
+
+/*
+ * Bit position definitions I2C_OAR1
+ */
+#define I2C_OAR1_ADD0    				 0
+#define I2C_OAR1_ADD71 				 	 1
+#define I2C_OAR1_ADD98  			 	 8
+#define I2C_OAR1_ADDMODE   			 	15
+
+/*
+ * Bit position definitions I2C_SR1
+ */
+
+#define I2C_SR1_SB 					 	0
+#define I2C_SR1_ADDR 				 	1
+#define I2C_SR1_BTF 					2
+#define I2C_SR1_ADD10 					3
+#define I2C_SR1_STOPF 					4
+#define I2C_SR1_RXNE 					6
+#define I2C_SR1_TXE 					7
+#define I2C_SR1_BERR 					8
+#define I2C_SR1_ARLO 					9
+#define I2C_SR1_AF 					 	10
+#define I2C_SR1_OVR 					11
+#define I2C_SR1_TIMEOUT 				14
+
+/*
+ * Bit position definitions I2C_SR2
+ */
+#define I2C_SR2_MSL						0
+#define I2C_SR2_BUSY 					1
+#define I2C_SR2_TRA 					2
+#define I2C_SR2_GENCALL 				4
+#define I2C_SR2_DUALF 					7
+
+/*
+ * Bit position definitions I2C_CCR
+ */
+#define I2C_CCR_CCR 					 0
+#define I2C_CCR_DUTY 					14
+#define I2C_CCR_FS  				 	15
+
+/*
+ * Macros to reset SPIx peripherals
+ */
+#define I2C1_REG_RESET()      do{ (RCC->APB1ENR |= (1 << 21)); (RCC->APB1RSTR &= ~(1 << 21)); }while(0)
+#define I2C2_REG_RESET()      do{ (RCC->APB1ENR |= (1 << 22)); (RCC->APB1RSTR &= ~(1 << 22)); }while(0)
+#define I2C3_REG_RESET()      do{ (RCC->APB1ENR |= (1 << 23)); (RCC->APB1RSTR &= ~(1 << 23)); }while(0)
+
+/******************************************************************************************
+ *Bit position definitions of USART peripheral
+ ******************************************************************************************/
+
+/*
+ * Bit position definitions USART_CR1
+ */
+#define USART_CR1_SBK					0
+#define USART_CR1_RWU 					1
+#define USART_CR1_RE  					2
+#define USART_CR1_TE 					3
+#define USART_CR1_IDLEIE 				4
+#define USART_CR1_RXNEIE  				5
+#define USART_CR1_TCIE					6
+#define USART_CR1_TXEIE					7
+#define USART_CR1_PEIE 					8
+#define USART_CR1_PS 					9
+#define USART_CR1_PCE 					10
+#define USART_CR1_WAKE  				11
+#define USART_CR1_M 					12
+#define USART_CR1_UE 					13
+#define USART_CR1_OVER8  				15
+
+
+
+/*
+ * Bit position definitions USART_CR2
+ */
+#define USART_CR2_ADD   				0
+#define USART_CR2_LBDL   				5
+#define USART_CR2_LBDIE  				6
+#define USART_CR2_LBCL   				8
+#define USART_CR2_CPHA   				9
+#define USART_CR2_CPOL   				10
+#define USART_CR2_STOP   				12
+#define USART_CR2_LINEN   				14
+
+
+/*
+ * Bit position definitions USART_CR3
+ */
+#define USART_CR3_EIE   				0
+#define USART_CR3_IREN   				1
+#define USART_CR3_IRLP  				2
+#define USART_CR3_HDSEL   				3
+#define USART_CR3_NACK   				4
+#define USART_CR3_SCEN   				5
+#define USART_CR3_DMAR  				6
+#define USART_CR3_DMAT   				7
+#define USART_CR3_RTSE   				8
+#define USART_CR3_CTSE   				9
+#define USART_CR3_CTSIE   				10
+#define USART_CR3_ONEBIT   				11
+
+/*
+ * Bit position definitions USART_SR
+ */
+
+#define USART_SR_PE        				0
+#define USART_SR_FE        				1
+#define USART_SR_NE        				2
+#define USART_SR_ORE       				3
+#define USART_SR_IDLE       			4
+#define USART_SR_RXNE        			5
+#define USART_SR_TC        				6
+#define USART_SR_TXE        			7
+#define USART_SR_LBD        			8
+#define USART_SR_CTS        			9
 
 // some generic Macros
 #define ENABLE           1
@@ -444,5 +632,8 @@ typedef struct
 
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
+#include "stm32f407xx_i2c_driver.h"
+#include "stm32f407xx_uart_driver.h"
+#include "stm32f407xx_rcc_driver.h"
 
 #endif /* INC_STM32F407XX_H_ */
